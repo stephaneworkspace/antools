ceb285d97f5814fa44a2e4aa5987c3fbe5ae83d9
Stéphane Bressani <stephane@bressani.dev>
2022-10-03 20:59:36 +0200

    fonction c++ fonctionne avec alignement de structure rust

--- exe/main.cpp
+++ exe/main.cpp
@@ -3,6 +3,8 @@
 //
 #include <fstream>
 #include <iostream>
+#include <vector>
+#include <stdio.h>
 #include "../include/antools.h"
 #include "../other_cmake/SweInterface/base64.h"
 
@@ -74,6 +76,124 @@ int svg() {
 
     svg_rust(data, point, i, j, path);
 }
+
+int get_size(std::string s, char c) {
+    return std::count(s.begin(), s.end(), c);
+}
+
+// TODO try catch sinon Struct erreur
+int get_value(std::string s, int pos) {
+    std::string c = ";";
+    auto start = 0U;
+    auto end = s.find(c);
+    int i = 0;
+    while (end != std::string::npos)
+    {
+        if (pos == i) {
+            //std::cout << s.substr(start, end - start) << std::endl;
+            return std::stoi(s.substr(start, end - start));
+        }
+        i++;
+        start = end + c.length();
+        end = s.find(c, start);
+    }
+    if (pos == i) {
+        //std::cout << s.substr(start, end) << std::endl;
+        return std::stoi(s.substr(start, end));
+    }
+}
+
+SvgData set_data(char c, int point_idx, int point_size) {
+    SvgData data;
+    data.point_idx = point_idx;
+    data.point_size = point_size;
+    data.data = c;
+    return data;
+}
+
+SvgPoint set_point(int point_idx, std::string point_string, int pos) {
+    SvgPoint point;
+    point.point_idx = point_idx;
+    point.point = get_value(point_string, pos);
+    return point;
+}
+
+int svg2() {
+    int i = 0;
+    int j = 0;
+    int size = 0;
+    std::string s = "";
+    char c = ' ';
+    vector<SvgData> vec_data;
+    vector<SvgPoint> vec_point;
+
+
+    //
+    c = 'M';
+    s = "10;10;";
+    size = get_size(s, ';');
+    vec_data.push_back(set_data(c, i, size));
+    for (int k = 0; k < size; ++k) {
+       vec_point.push_back(set_point(i, s, k));
+    }
+    i++;
+    j += size;
+
+    //
+    c = 'l';
+    s = "0;50;";
+    size = get_size(s, ';');
+    vec_data.push_back(set_data(c, i, size));
+    for (int k = 0; k < size; ++k) {
+        vec_point.push_back(set_point(i, s, k));
+    }
+    i++;
+    j += size;
+
+    //
+    c = 'l';
+    s = "50;0;";
+    size = get_size(s, ';');
+    vec_data.push_back(set_data(c, i, size));
+    for (int k = 0; k < size; ++k) {
+        vec_point.push_back(set_point(i, s, k));
+    }
+    i++;
+    j += size;
+
+    //
+    c = 'l';
+    s = "0;-50;";
+    size = get_size(s, ';');
+    vec_data.push_back(set_data(c, i, size));
+    for (int k = 0; k < size; ++k) {
+        vec_point.push_back(set_point(i, s, k));
+    }
+    i++;
+    j += size;
+
+    //
+    c = 'z';
+    size = 0;
+    vec_data.push_back(set_data(c, i, size));
+    i++;
+    j += size;
+
+    SvgData* data = vec_data.data();
+    SvgPoint* point = vec_point.data();
+    /*
+<svg viewBox="0 0 70 70" xmlns="http://www.w3.org/2000/svg">
+<path d="M10,10 l0,50 l50,0 l0,-50 z" fill="none" stroke="black" stroke-width="3"/>
+</svg>
+     */
+
+    string p = filesystem::current_path().parent_path().parent_path();
+    p += "/temp/svg_rust.svg";
+    const char* path = p.c_str();
+
+    svg_rust(data, point, i, j, path);
+}
+
 int main() {
     /*
     string path_string_log = filesystem::current_path().parent_path().parent_path();
@@ -83,7 +203,7 @@ int main() {
     const char* log_error_p = log_error.data();
     log_app(log_output_p, log_error_p);
     */
-    svg();
+    svg2();
     cout << "Hello, World!" << endl;
 
     // Créer un pdf du thème astral dans /temp/mypdf.pdf
