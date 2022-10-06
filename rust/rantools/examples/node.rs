6bf62a06c6f745167ff33b612d2612fbebadfe9d
Stéphane Bressani <stephane@bressani.dev>
2022-10-06 20:40:02 +0200

    trouvé un crate idéal au lieu de tout reécrire

--- rust/rantools/examples/node.rs
+++ rust/rantools/examples/node.rs
@@ -1,6 +1,5 @@
 use std::env;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::fs;
 use std::path::PathBuf;
 
 fn main() {
@@ -28,18 +27,41 @@ pub enum Node {
     Div(Vec<Attr>,)
 }
 
-pub(crate) fn node_to_pdf() -> std::io::Result<()> {
-
+pub(crate) fn node_to_pdf() -> Result<(), String> {
     let mut file_path = PathBuf::new();
     file_path.push(env::current_dir().unwrap().as_path());
     file_path.push("examples");
     file_path.push("template.an");
-    let file = File::open(file_path.as_path())?;
-    let reader = BufReader::new(&file);
-    let mut template: String = "".to_string();
-    for l in reader.lines() {
-        template = format!("{}{}", template, l.unwrap());
+
+    let text = fs::read_to_string(file_path.clone()).unwrap();
+/*
+    let doc = roxmltree::Document::parse(&text).unwrap();
+
+    println!("Elements count: {}",
+             doc.root().descendants().filter(|n| n.is_element()).count());
+
+    let attrs_count: usize = doc.root().descendants().map(|n| n.attributes().len()).sum();
+    println!("Attributes count: {}", attrs_count);
+
+    let ns_count: usize = doc.root().descendants().map(|n| n.namespaces().len()).sum();
+*/
+    // Allow DTD for this example.
+    let opt = roxmltree::ParsingOptions {
+        allow_dtd: true,
+    };
+
+    let doc = match roxmltree::Document::parse_with_options(&text, opt) {
+        Ok(doc) => {
+            //print!("{:?}", &doc);
+            doc
+        },
+        Err(e) => return Err(format!("Error: {}.", e).to_string()),
+    };
+
+    let _vec_node: Vec<Node> = Vec::new();
+    for x in doc.root().descendants().filter(|n| n.is_element()) {
+        print!("{:?}", x);
     }
-    println!("{}", template);
+
     Ok(())
 }
